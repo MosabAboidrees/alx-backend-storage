3037f1805e2fd89ac13b90790407aa3913348aa7
MosabAboidrees <mosab_mahala@yahoo.com>
2024-10-18 01:07:41 +0300

    Task 4. Buy buy buy

---
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -0,0 +1,10 @@
+-- Task: Create a trigger that decreases the quantity of an item after adding a new order
+
+DELIMITER //
+
+CREATE TRIGGER decrease_quantity_after_order
+AFTER INSERT ON orders
+FOR EACH ROW
+UPDATE items
+SET quantity = quantity - NEW.number
+WHERE name = NEW.item_name;
