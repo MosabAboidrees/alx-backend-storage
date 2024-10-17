b9a2051014d53a36826cabf14bbad885006b5a88
MosabAboidrees <mosab_mahala@yahoo.com>
2024-10-18 00:08:55 +0300

    Task 0. We are all unique!

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,9 @@
+-- Script to create a table `users` with unique email attribute
+
+-- Create table `users` if it does not exist
+CREATE TABLE IF NOT EXISTS users (
+	id INT NOT NULL AUTO_INCREMENT,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255),
+	PRIMARY KEY (id)
+);
