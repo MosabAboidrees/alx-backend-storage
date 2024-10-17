16a9b470aa2c05f289e2fb79edf1adc4e2a9a8c7
MosabAboidrees <mosab_mahala@yahoo.com>
2024-10-18 01:26:23 +0300

    Task 8. Optimize simple search

--- 0x00-MySQL_Advanced/8-index_my_names.sql
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -1,6 +1,5 @@
--- 9. Optimize search and score
--- This SQL script creates an index idx_name_first_score on the table names
--- The index is created on the first letter of the name and the score
+-- Create an index on the 'name' column of the 'names' table
+-- The index will use only the first character of the 'name' column
 
 CREATE INDEX idx_name_first
 ON names (name(1));
