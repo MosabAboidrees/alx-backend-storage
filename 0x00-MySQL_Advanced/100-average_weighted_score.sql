a697343c6ff547234a7c47441890b1dc7f313d1f
MosabAboidrees <mosab_mahala@yahoo.com>
2024-10-18 01:55:02 +0300

    Task 12. Average weighted score

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -1,9 +1,9 @@
 -- ComputeAverageWeightedScoreForUser stored procedure
 -- This procedure computes and stores the average weighted score for a student
 
-DELIMITER $$
 DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUser;
-CREATE PROCEDURE ComputeAverageWeightedScoreForUser(IN user_id INT)
+DELIMITER $$
+CREATE PROCEDURE ComputeAverageWeightedScoreForUser (IN user_id INT)
 BEGIN
 	-- Update the user's average score based on the average weighted score
 	-- of the quizzes they have taken
@@ -14,6 +14,6 @@ BEGIN
 		INNER JOIN projects ON projects.id = corrections.project_id
 		WHERE corrections.user_id = user_id
 	)
-	WHERE user.id = user_id;
+	WHERE users.id = user_id;
 END $$
 DELIMITER ;
