587acb46938a4558eb5d5fedf90c0c6742df4361
MOSAB ABOIDREES ALTRAIFI YOUSIF <33727845+MosabAboidrees@users.noreply.github.com>
2024-10-23 03:55:39 +0300

    Update 12-log_stats.py

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,38 +1,31 @@
 #!/usr/bin/env python3
-'''Task 12's module: Provides some stats about Nginx logs stored in MongoDB.
-'''
+"""Log stats"""
+
 from pymongo import MongoClient
 
 
-def print_nginx_request_logs(nginx_collection):
-    '''Prints stats about Nginx request logs.
-    '''
-    # Total number of logs
-    print('{} logs'.format(nginx_collection.count_documents({})))
-
-    # Methods stats
-    print('Methods:')
-    methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
-    for method in methods:
-        req_count = nginx_collection.count_documents({'method': method})
-        print('\tmethod {}: {}'.format(method, req_count))
-
-    # GET requests to /status
-    status_checks_count = nginx_collection.count_documents({
-        'method': 'GET', 'path': '/status'
-    })
-    print('{} status check'.format(status_checks_count))
-
-
-def run():
-    '''Provides some stats about Nginx logs stored in MongoDB.
-    '''
-    # Connect to the MongoDB server
+def log_stats():
+    """ log_stats.
+    """
     client = MongoClient('mongodb://127.0.0.1:27017')
+    logs_collection = client.logs.nginx
+    total = logs_collection.count_documents({})
+    get = logs_collection.count_documents({"method": "GET"})
+    post = logs_collection.count_documents({"method": "POST"})
+    put = logs_collection.count_documents({"method": "PUT"})
+    patch = logs_collection.count_documents({"method": "PATCH"})
+    delete = logs_collection.count_documents({"method": "DELETE"})
+    path = logs_collection.count_documents(
+        {"method": "GET", "path": "/status"})
+    print(f"{total} logs")
+    print("Methods:")
+    print(f"\tmethod GET: {get}")
+    print(f"\tmethod POST: {post}")
+    print(f"\tmethod PUT: {put}")
+    print(f"\tmethod PATCH: {patch}")
+    print(f"\tmethod DELETE: {delete}")
+    print(f"{path} status check")
 
-    # Call the function to print stats
-    print_nginx_request_logs(client.logs.nginx)
 
-
-if __name__ == '__main__':
-    run()
+if __name__ == "__main__":
+    log_stats()
