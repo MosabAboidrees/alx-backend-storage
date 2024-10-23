18a2d79815933805af23aa0f591ed18a1b3d9618
MOSAB ABOIDREES ALTRAIFI YOUSIF <33727845+MosabAboidrees@users.noreply.github.com>
2024-10-23 03:21:21 +0300

    Update Task 12. Log stats

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -13,22 +13,22 @@ def log_stats():
     # Connect to MongoDB
     client = MongoClient('mongodb://127.0.0.1:27017').logs.nginx
 
-    # Nginx logs
-    print(f"{client.estimated_document_count()} logs")
-
     # Get total number of logs
-    total_logs = client.count_documents({})
+    n_logs = client.count_documents({})
+    print(f'{n_logs} logs')
 
     # Get count for each HTTP method
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-
+    
     print('Methods:')
     for method in methods:
         method_counts = client.count_documents({"method": method})
         print(f'\tmethod {method}: {method_counts}')
 
     # Get count of GET requests to /status path
-    status_check_count = client.count_documents({"method": "GET", "path": "/status"})
+    status_check_count = client.count_documents(
+        {"method": "GET", "path": "/status"}
+    )
     print(f"{status_check_count} status check")
 
 if __name__ == "__main__":
